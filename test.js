91c0b6011cc0cb02cd2f6da5bbc3b8d1c586bf5b
startling <tdixon51793@gmail.com>
2013-06-20 21:46:48 -0400

    test reading ascii PBM, + helper function

--- test.js
+++ test.js
@@ -1,10 +1,31 @@
+var fs = require("fs");
 var assert = require("assert");
 var mask = require("./mask");
+
+function readToArrayBuffer (path, callback) {
+  var rs = fs.createReadStream(path);
+  rs.on("readable", function () {
+    var buffer = rs.read();
+    var ab = new ArrayBuffer(buffer.length);
+    var u8 = new Uint8Array(ab);
+    for (var i = 0; i < buffer.length; i++) {
+      u8[i] = buffer[i];
+    };
+    callback(ab);
+  });
+};
 
 describe("mask.Mask", function () {
   describe("#from_pbm()", function () {
     it("exists", function () {
       assert.notEqual(mask.Mask.from_pbm, undefined);
+    });
+    it("reads test-data/bullet-ascii.pbm.", function (done) {
+      readToArrayBuffer("test-data/bullet-ascii.pbm", function (ab) {
+        mask.Mask.from_pbm(ab, function (mask) {
+          done();
+        });
+      });
     });
   });
 });
