236bc471cd21cab5b5d7f8903c381f3aca352897
startling <tdixon51793@gmail.com>
2013-06-21 04:08:48 -0400

    simplified things in the tests

--- test.js
+++ test.js
@@ -11,7 +11,13 @@ function readToArrayBuffer (path, callback) {
     for (var i = 0; i < buffer.length; i++) {
       u8[i] = buffer[i];
     };
-    callback(ab);
+    return callback(ab);
+  });
+};
+
+function readMask (path, callback) {
+  return readToArrayBuffer (path, function (ab) {
+    return mask.Mask.fromPBM(ab, callback);
   });
 };
 
@@ -27,21 +33,17 @@ describe("mask.Mask", function () {
     });
     function assertReads (path) {
       it("reads " + path, function (done) {
-        readToArrayBuffer(path, function (ab) {
-          mask.Mask.fromPBM(ab, function (mask) {
-            done();
-          });
+        readMask(path, function (mask) {
+          done();
         });
       });
     };
     function assertSize (path, width, height) {
       it("gets the size of " + path + " right", function (done) {
-        readToArrayBuffer(path, function (ab) {
-          mask.Mask.fromPBM(ab, function (mask) {
-            assert.equal(mask.size.x, width);
-            assert.equal(mask.size.y, height);
-            done();
-          });
+        readMask(path, function (mask) {
+          assert.equal(mask.size.x, width);
+          assert.equal(mask.size.y, height);
+          done();
         });
       });
     };
@@ -51,20 +53,16 @@ describe("mask.Mask", function () {
     });
     function assertDataEqual (a, b) {
       it("gets the same data for " + a + " and " + b, function (done) {
-        readToArrayBuffer(a, function (abA) {
-          mask.Mask.fromPBM(abA, function (maskA) {
-            readToArrayBuffer(b, function (abB) {
-              mask.Mask.fromPBM(abB, function (maskB) {
-                assert.equal(maskA.size.x, maskB.size.x);
-                assert.equal(maskA.size.y, maskB.size.y);
-                for (var x = 0; x < maskA.size.x; x++) {
-                  for (var y = 0; y < maskA.size.y; y++) {
-                    assert.equal(maskA.data[x][y], maskB.data[x][y]);
-                  };
-                };
-                done();
-              });
-            });
+        readMask(a, function (maskA) {
+          readMask(b, function (maskB) {
+            assert.equal(maskA.size.x, maskB.size.x);
+            assert.equal(maskA.size.y, maskB.size.y);
+            for (var x = 0; x < maskA.size.x; x++) {
+              for (var y = 0; y < maskA.size.y; y++) {
+                assert.equal(maskA.data[x][y], maskB.data[x][y]);
+              };
+            };
+            done();
           });
         });
       });
@@ -77,33 +75,27 @@ describe("mask.Mask", function () {
   describe(".collidesWith()", function () {
     function assertCollidesWithSelf (path) {
       it ("says " + path + " collides with itself", function (done) {
-        readToArrayBuffer(path, function (ab) {
-          mask.Mask.fromPBM(ab, function (m) {
-            assert.equal(mask.Mask.collision(m, m), true);
-            done();
-          });
+        readMask(path, function (m) {
+          assert.equal(mask.Mask.collision(m, m), true);
+          done();
         });
       });
     };
     function assertWithinSelf (path) {
       it ("says " + path + " is within itself", function (done) {
-        readToArrayBuffer(path, function (ab) {
-          mask.Mask.fromPBM(ab, function (m) {
-            assert.equal(mask.Mask.within(m, m), true);
-            done();
-          });
+        readMask(path, function (m) {
+          assert.equal(mask.Mask.within(m, m), true);
+          done();
         });
       });
     };
     function assertDoesNotCollideWithTranslatedSelf (path) {
       it("says " + path + "doesn't collide with itself, translated",
          function (done) {
-           readToArrayBuffer(path, function (ab) {
-             mask.Mask.fromPBM(ab, function (m) {
-               var o = m.at(m.size.x, m.size.y);
-               assert.equal(mask.Mask.collision(m, o), false);
-               done();
-             });
+           readMask(path, function (m) {
+             var o = m.at(m.size.x, m.size.y);
+             assert.equal(mask.Mask.collision(m, o), false);
+             done();
            });
          });
     };
