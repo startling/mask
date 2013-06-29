70ec1d3d456c69af92232b4c01dc11ddb32c968d
startling <tdixon51793@gmail.com>
2013-06-29 07:15:00 -0400

    more benchmarks

--- benchmark.js
+++ benchmark.js
@@ -33,13 +33,39 @@ readMask("test-data/frame-10-10-ascii.pbm",
                        diff.change.size + ".");
          });
 
-readBoth("test-data/frame-10-10-ascii.pbm",
-         "test-data/dot-10-10-ascii.pbm",
-         function (a_, b_) {
-           new Benchmark.Suite().add("Mask.PBM#collidesAt", function () {
-             Mask.collision(a_, b_);
-           }).on("cycle", function (event) {
-             console.log(String(event.target));
-           }).run();
-         });
-
+var frame = "test-data/frame-10-10-ascii.pbm";
+var dot = "test-data/dot-10-10-ascii.pbm";
+readBoth(frame, dot, function (frame_, dot_) {5
+  new Benchmark.Suite().
+    add("Mask.collision [frame@0/0, dot@0/0 ]", function () {
+      Mask.collision(frame_, dot_);
+    }).
+    add("Mask.collision [frame@9/9, dot@0/0 ]", function () {
+      Mask.collision(frame_.at(9, 9), dot_);
+    }). 
+    add("Mask.collision [dot@0/0, frame@9/9 ]", function () {
+      Mask.collision(dot_, frame_.at(9, 9));
+    }).
+    add("Mask.collision [frame@0/0, bounding]", function () {
+      Mask.collision(frame_, Mask.Box.bounding(frame_));
+    }).
+    add("Mask.collision [dot@0/0, bounding  ]", function () {
+      Mask.collision(dot_, Mask.Box.bounding(dot_));
+    }).
+    add("Mask.collision [10/10@0/0, 2/2@0/0 ]", function () {
+      Mask.collision(new Mask.Box(10, 10),
+                     new Mask.Box(2, 2));
+    }).
+    add("Mask.collision [2/2@0/0, 10/10@0/0 ]", function () {
+      Mask.collision(new Mask.Box(2, 2),
+                     new Mask.Box(10, 10));
+    }).
+    add("Mask.collision [2/2@2/2, 4/4@0/0   ]", function () {
+      Mask.collision(new Mask.Box(2, 2).at(2, 2),
+                     new Mask.Box(4, 4));
+    }).
+    on("cycle", function (event) {
+     console.log(event.target.toString());
+    }).
+    run();
+});
